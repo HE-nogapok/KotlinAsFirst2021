ae3349d2157f86b29af34e94a9c7af38fbd11708
HEnogapok <mr.prezent@mail.ru>
2022-05-13 07:32:22 +0300

    Lesson2 Task2.circleInside

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -67,7 +67,7 @@ fun daysInMonth(month: Int, year: Int): Int {
 fun circleInside(
     x1: Double, y1: Double, r1: Double,
     x2: Double, y2: Double, r2: Double
-): Boolean = TODO()
+): Boolean = r1<=r2 && (((x1-x2)*(x1-x2))+((y1-y2)*(y1-y2))) <= ((r2-r1)*(r2-r1))
 
 /**
  * Средняя (3 балла)
